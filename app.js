6fedc3e0e08c38ba148ed5b04f30e0be08707fe4
darmaa <dddarmaa@gmail.com>
2024-09-08 15:58:43 +0800

    new game zassan

--- app.js
+++ app.js
@@ -89,25 +89,29 @@ document.querySelector(".btn-new").addEventListener("click", NewGame);
 
 function NewGame(){
     isNewGame = true ; 
+
+
 acttivePlayer =0 ;
 scores = [0 , 0];
 roundScore = 0 ;
 
 
+
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent= "0";
-document.getElementById("current-1").textContent= "0";
-diceDom.style.display = "none";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.getElementById("name-0").textContent="Player 1";
-document.getElementById("name-1").textContent="Player 2";
+document.getElementById("name-0").textContent = "Player 1";
+document.getElementById("name-1").textContent = "Player 2";
 
 document.querySelector(".player-0-panel").classList.remove("winner");
 document.querySelector(".player-1-panel").classList.remove("winner");
 
+document.querySelector(".player-0-panel").classList.remove("active");
+document.querySelector(".player-1-panel").classList.remove("active");
 
-document.querySelector(".player-0-panel").classList.remove("active");
-document.querySelector(".player-0-panel").classList.remove("active");
 document.querySelector(".player-0-panel").classList.add("active");
+
+diceDom.style.display = "none";
 }
